963e7740e046cc29c79544128e7be724b34e031d
Lee Short <blackcat@pro-ns.net>
2021-03-24 13:36:17 -0700

    Optimizing compilations

--- clang_test.bash
+++ clang_test.bash
@@ -1,3 +1,3 @@
 ulimit -s unlimited
-clang++-12 vector_speed_test.cpp -o clang_vector_speed_test
+clang++-12 -O3 vector_speed_test.cpp -o clang_vector_speed_test
 ./clang_vector_speed_test 2>&1 | tee ./clang_test_results.txt
--- gcc_test.bash
+++ gcc_test.bash
@@ -1,3 +1,3 @@
 ulimit -s unlimited
-g++-10 vector_speed_test.cpp -o gcc_vector_speed_test
+g++-10 -O3 vector_speed_test.cpp -o gcc_vector_speed_test
 ./gcc_vector_speed_test 2>&1 | tee ./gcc_test_results.txt
